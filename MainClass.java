3861aa21cc257ac1d22ebef92c9acde4bf01657e
Sebastian Velez <sebasv89@gmail.com>
2013-04-17 15:31:43 -0500

    Adding multiply method

--- MainClass.java
+++ MainClass.java
@@ -5,7 +5,12 @@ public class MainClass {
 		return a + b; 
 	}
 	
+	public static int multiply(int a, int b){
+		return a * b; 
+	}
+	
 	public static void main(String[] args){
 		System.out.println(add(3,2));
+		System.out.println(multiply(3,2));
 	}
 }

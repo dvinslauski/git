cffa23b999e483f55452500faea9fe6b87ce501b
Sebastian Velez <sebasv89@gmail.com>
2013-04-17 15:35:29 -0500

    Add Pow operation

--- MainClass.java
+++ MainClass.java
@@ -9,8 +9,13 @@ public class MainClass {
 		return a * b; 
 	}
 	
+	public static int pow(int a, int b){
+		return (int) Math.pow(a, b); 
+	}
+	
 	public static void main(String[] args){
 		System.out.println(add(3,2));
 		System.out.println(multiply(3,2));
+		System.out.println(pow(3,2));
 	}
 }
